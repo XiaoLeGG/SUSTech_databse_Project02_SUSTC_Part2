b065d571f470cf75d381eeb34474e50cdf519916
Maystern <12112910@mail.sustech.edu.cn>
2022-12-20 13:28:36 +0800

    通过所有测试 21/21

--- src/src/main/java/main/Main.java
+++ src/src/main/java/main/Main.java
@@ -1,14 +1,16 @@
 package main;
 
 public class Main {
-	private static String url = "jdbc:postgresql://localhost:5432/cslab1";
-	private static String user = "test";
-	private static String pass = "123456";
+	private static String url = "localhost:5432/postgres";
+
+	private static String user = "postgres";
+	private static String pass = "ljcfyh_123@99";
 	
 	private static ThrowableHandler th;
 
 	public static void main(String[] args) {
 		DBManipulation databaseManager = new DBManipulation(url, user, pass);
+
 		databaseManager.$import("data/records.csv", "data/staffs.csv");
 		th = new ThrowableHandler();
 	}
